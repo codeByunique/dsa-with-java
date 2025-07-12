beed4aeadbc2f15af71c9e17c3365184148a13da
Atabul <144981177+codeByunique@users.noreply.github.com>
2025-07-12 18:55:31 +0530

    Create FindMaxMin.java

---
+++ 01-arrays/FindMaxMin.java
@@ -0,0 +1,20 @@
+// Problem: Find Maximum and Minimum in an Array
+// Author: Ataubl (codeByunique)
+
+public class FindMaxMin {
+    public static void main(String[] args) {
+        int[] arr = {3, 9, 1, 7, 4, -2, 10};
+        int n = arr.length;
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+    }
+}
