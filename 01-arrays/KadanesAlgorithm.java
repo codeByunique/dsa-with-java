645d50854d7749a1ee56d1e7036bf2afd80b0aa8
Atabul <144981177+codeByunique@users.noreply.github.com>
2025-07-12 18:57:06 +0530

    Create KadanesAlgorithm.java

---
+++ 01-arrays/KadanesAlgorithm.java
@@ -0,0 +1,18 @@
+// Problem: Maximum Subarray Sum (Kadane's Algorithm)
+// Author: Ataubl (codeByunique)
+
+public class KadanesAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int maxSoFar = arr[0];
+        int currentSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSoFar = Math.max(maxSoFar, currentSum);
+        }
+
+        System.out.println("Maximum Subarray Sum: " + maxSoFar);
+    }
+}
